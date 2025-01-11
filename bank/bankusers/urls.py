e5f5b29c3c3f91eecab0fb16b45c0f22aa00c4be
ajaye2901 <ajaye2901@gmail.com>
2025-01-11 11:21:17 +0530

    registered bankstaff and customer into admin panel

--- bank/bankusers/urls.py
+++ bank/bankusers/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('admin-login/', SuperAdminLogin.as_view(), name='admin-login'),
-    path('staff-regsiter/', BankStaffRegistrationView.as_view(), name='staff-register'),
+    path('staff-register/', BankStaffRegistrationView.as_view(), name='staff-register'),
     path('staff-login/', BankStaffLoginView.as_view(), name='staff-login'),
     path('customer-register/', CustomerRegistrationView.as_view(), name='customer-register'),
     path('customer-login/', CustomerLoginView.as_view(), name='customer-login'),
